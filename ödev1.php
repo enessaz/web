58c330a392c2074f7c798a6e5c25430572be44ff
enessaz <100769399+enessaz@users.noreply.github.com>
2022-04-13 13:56:08 +0300

    Add files via upload

---
+++ ödev1.php
@@ -0,0 +1,58 @@
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ders4</title>
+</head>
+<body>
+    
+
+
+   <?php
+    
+    /*Rastgele üretilen sayı tekmi çiftmi 
+
+$rastgele_sayı=rand(1,100);
+echo"rastgele sayı:". $rastgele_sayı;
+echo"<br>";
+if ($rastgele_sayı%2==0) {
+    echo"Rastgele Sayı:". $rastgele_sayı;
+    echo"<br>";
+    echo"Rastgele sayı çifttir";
+    
+}
+else    {
+    
+    echo"<br>";
+    echo"Rastgele sayı Tektir";
+   
+}
+
+*/
+/*
+$rastgele=rand(1,100);
+if ($rastgele>50) {
+    echo"Öğrenci Notu:" . $rastgele;
+    echo"<br>";
+    echo"Geçtiniz";
+} 
+
+else{
+    echo"Öğrenci Notu:" . $rastgele;
+    echo"<br>";
+    echo"Kaldınız";
+}
+*/
+
+
+
+
+
+
+
+              ?>
+
+</body>
+
+</html>
